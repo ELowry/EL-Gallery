d423733b5917b85921c0f70424ef546cbc07dc6c
Eric Lowry <ericlowry14@gmail.com>
2014-09-23 12:03:31 +0200

    Version 1.2.7

--- el-gallery/js/el-gallery_admin.js
+++ el-gallery/js/el-gallery_admin.js
@@ -17,4 +17,10 @@ License: GPL2
 		});
 	});
 
+	$(document).ready(function() {
+		$('#wpbody-content .wrap h2:first-child').text(function () {
+			return $(this).text().replace('EL', '<i class="el-icons el-icons-el-gallery" class="el-icons-logo"><div>EL</div></i>'); 
+		});​​​​​
+	});​​​​​
+
 })( jQuery );
